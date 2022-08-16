81f80aa9a0c4fad61d7ceb5973d0dedf9453394c
Sergio Ricardo de Sousa <sergio.ricardo.sousa@avanade.com>
2022-08-16 18:14:41 -0300

    env: include variable DATABASE_URL for production

--- src/apirest.WebApi/Startup.cs
+++ src/apirest.WebApi/Startup.cs
@@ -66,7 +66,7 @@ namespace apirest.WebApi
                 databaseUrl = Configuration.GetConnectionString("DefaultConnection");
             } else
             {
-                databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+                databaseUrl = GetHerokuConnectionString();
             }
 
             Action<DbContextOptionsBuilder> options = options =>
@@ -98,6 +98,21 @@ namespace apirest.WebApi
             {
                 endpoints.MapControllers();
             });
+        }
+
+        static string GetHerokuConnectionString()
+        {
+            string connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            var databaseUri = new Uri(connectionUrl);
+
+            string db = databaseUri.LocalPath.TrimStart('/');
+
+            string[] userInfo = databaseUri.UserInfo
+                                .Split(':', StringSplitOptions.RemoveEmptyEntries);
+
+            return $"User ID={userInfo[0]};Password={userInfo[1]};Host={databaseUri.Host};" +
+                   $"Port={databaseUri.Port};Database={db};Pooling=true;" +
+                   $"SSL Mode=Require;Trust Server Certificate=True;";
         }
     }
 }
